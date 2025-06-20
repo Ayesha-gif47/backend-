80b24b29cf50fc5e2054ebe28eb511a817c30446
Ayesha Younas <ayeshayounas982@gmail.com>
2025-06-21 00:33:45 +0500

    Update Patient.cs

--- hope4life/Models/Entities/Patient.cs
+++ hope4life/Models/Entities/Patient.cs
@@ -1,5 +1,4 @@
-﻿
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace hope4life.Models.Entities
 {
@@ -42,44 +41,3 @@ namespace hope4life.Models.Entities
         public DateTime? UpdatedOn { get; set; }
     }
 }
-/*using System;
-using System.ComponentModel.DataAnnotations;
-
-namespace hope4life.Models.Entities   // ⬅ DbContext یہی namespace استعمال کرے گا
-{
-    public class Patient
-    {
-        public Guid Id { get; set; }
-
-        [Required]
-        public string Name { get; set; } = string.Empty;
-
-        public string Address { get; set; } = string.Empty;
-
-        [Phone]
-        public string PhoneNumber { get; set; } = string.Empty;
-
-        public string GuardianName { get; set; } = string.Empty;
-        public string Relationship { get; set; } = string.Empty;
-
-        [EmailAddress]
-        public string Email { get; set; } = string.Empty;
-
-        [Required]
-        public string Password { get; set; } = string.Empty;
-
-        public string BloodType { get; set; } = string.Empty;
-
-        /// <summary>
-        /// 2 = ہر دو ہفتے، 4 = ہر چار ہفتے۔
-        /// </summary>
-        [Required]
-        [Range(1, 52, ErrorMessage = "FrequencyInWeeks must be 1‑52.")]
-        public int FrequencyInWeeks { get; set; }
-
-        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
-        public DateTime? UpdatedOn { get; set; }
-    }
-}*/
-
-
